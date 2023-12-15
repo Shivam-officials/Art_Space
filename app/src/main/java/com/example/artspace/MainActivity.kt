5aca44ae5750704486b0512473486287a7988e2d
Shivam <suryabhardwajofficial@gmail.com>
2023-12-15 13:51:58 +0530

    imported the required drawables and made the imageCard composable for showing image

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -3,13 +3,26 @@ package com.example.artspace
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +35,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    AnimeApp()
                 }
             }
         }
@@ -30,17 +43,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AnimeApp() {
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        
+        val animeImage by remember {
+            mutableStateOf(R.drawable.kira_death_note_2006_2007)
+        }
+
+        ImageCard(animeImage)
+
+    }
+
 }
 
-@Preview(showBackground = true)
+//image card to show the image of the anime
 @Composable
-fun GreetingPreview() {
+private fun ImageCard(animeImage: Int) {
+    OutlinedCard(
+        colors = CardDefaults.cardColors(),
+        elevation = CardDefaults.cardElevation()
+    ) {
+        Image(
+            painter = painterResource(animeImage),
+            contentDescription = null,
+            modifier = Modifier.padding(10.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
     ArtSpaceTheme {
-        Greeting("Android")
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            AnimeApp()
+        }
     }
 }
