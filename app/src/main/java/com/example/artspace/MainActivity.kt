d962b01854aaff05e0ada3bc312b0fe00a06234b
Shivam <suryabhardwajofficial@gmail.com>
2023-12-15 16:05:44 +0530

    added the animeDescreptor composable for showing the anime title,studio and airing time

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -6,8 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
@@ -20,9 +26,17 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -47,17 +61,74 @@ fun AnimeApp() {
 
     Column(
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        
+        horizontalAlignment = Alignment.CenterHorizontally, content = function()
+    )
+
+
+}
+
+@Composable
+private fun function(): @Composable() (ColumnScope.() -> Unit) =
+    {
+
         val animeImage by remember {
             mutableStateOf(R.drawable.kira_death_note_2006_2007)
         }
 
+        val animeTitle by remember { mutableStateOf(R.string.death_note_title) }
+        val animeStudio by remember { mutableStateOf(R.string.death_note_studio) }
+        val animeAirTime by remember { mutableStateOf(R.string.death_note_airing_time) }
+
         ImageCard(animeImage)
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        AnimeDescreptor(animeTitle, animeStudio, animeAirTime)
+
+
 
     }
 
+@Composable
+private fun AnimeDescreptor(animeTitle: Int, animeStudio: Int, animeAirTime: Int) {
+    OutlinedCard(
+        colors = CardDefaults.cardColors(
+            Color(0xffCCCCCC)
+        ),
+
+        ) {
+        Column(
+            modifier = Modifier.padding(10.dp)
+        ) {
+
+            //anime Name
+            Text(
+                text = stringResource(id = animeTitle),
+                style = TextStyle.Default.copy(
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 20.sp
+                )
+            )
+
+            //Row for studio and airTime
+            Row {
+
+                //Anime studio name
+                Text(
+                    text = stringResource(id = animeStudio),
+                    style = TextStyle.Default.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
+
+                //anime airing time
+                Text(
+                    text = " (${stringResource(id = animeAirTime)})"
+                )
+            }
+
+        }
+    }
 }
 
 //image card to show the image of the anime
@@ -65,12 +136,13 @@ fun AnimeApp() {
 private fun ImageCard(animeImage: Int) {
     OutlinedCard(
         colors = CardDefaults.cardColors(),
-        elevation = CardDefaults.cardElevation()
+        elevation = CardDefaults.cardElevation(),
     ) {
         Image(
             painter = painterResource(animeImage),
             contentDescription = null,
-            modifier = Modifier.padding(10.dp)
+            modifier = Modifier
+                .padding(10.dp)
         )
     }
 }
